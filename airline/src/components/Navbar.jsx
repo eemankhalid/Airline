e7d0d917d0e9338a5595f9df1980ea2a8c5146b0
amnaahad <amnakhan200427@gmail.com>
2024-08-07 16:25:36 +0500

    Hero

--- airline/src/components/Navbar.jsx
+++ airline/src/components/Navbar.jsx
@@ -60,68 +60,22 @@ const Navbar = () => {
                     </div>
                 </div>
             </nav>
-            <div className="carousel-header">
-                <div id="carouselId" className="carousel slide" data-bs-ride="carousel">
-                    <ol className="carousel-indicators">
-                        <li data-bs-target="#carouselId" data-bs-slide-to="0" className="active"></li>
-                        <li data-bs-target="#carouselId" data-bs-slide-to="1"></li>
-                        <li data-bs-target="#carouselId" data-bs-slide-to="2"></li>
-                    </ol>
-                    <div className="carousel-inner" role="listbox">
-                        <div className="carousel-item active">
-                            <img src="img/carousel-2.jpg" className="img-fluid" alt="Image" />
-                            <div className="carousel-caption">
-                                <div className="p-3" style={{ maxWidth: '900px' }}>
-                                    <h4 className="text-white text-uppercase fw-bold mb-4" style={{ letterSpacing: '3px' }}>Explore The World</h4>
-                                    <h1 className="display-2 text-capitalize text-white mb-4">Let's The World Together!</h1>
-                                    <p className="mb-5 fs-5">
-                                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                                    </p>
-                                    <div className="d-flex align-items-center justify-content-center">
-                                        <Link className="btn-hover-bg btn btn-primary rounded-pill text-white py-3 px-5" to="#">Discover Now</Link>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="carousel-item">
-                            <img src="img/carousel-1.jpg" className="img-fluid" alt="Image" />
-                            <div className="carousel-caption">
-                                <div className="p-3" style={{ maxWidth: '900px' }}>
-                                    <h4 className="text-white text-uppercase fw-bold mb-4" style={{ letterSpacing: '3px' }}>Explore The World</h4>
-                                    <h1 className="display-2 text-capitalize text-white mb-4">Find Your Perfect Tour At Travel</h1>
-                                    <p className="mb-5 fs-5">
-                                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                                    </p>
-                                    <div className="d-flex align-items-center justify-content-center">
-                                        <Link className="btn-hover-bg btn btn-primary rounded-pill text-white py-3 px-5" to="#">Discover Now</Link>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="carousel-item">
-                            <img src="img/carousel-3.jpg" className="img-fluid" alt="Image" />
-                            <div className="carousel-caption">
-                                <div className="p-3" style={{ maxWidth: '900px' }}>
-                                    <h4 className="text-white text-uppercase fw-bold mb-4" style={{ letterSpacing: '3px' }}>Explore The World</h4>
-                                    <h1 className="display-2 text-capitalize text-white mb-4">You Like To Go?</h1>
-                                    <p className="mb-5 fs-5">
-                                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-                                    </p>
-                                    <div className="d-flex align-items-center justify-content-center">
-                                        <Link className="btn-hover-bg btn btn-primary rounded-pill text-white py-3 px-5" to="#">Discover Now</Link>
-                                    </div>
-                                </div>
-                            </div>
+            <div className="hero-video-container">
+                <video className="hero-video" autoPlay loop muted>
+                    <source src="src/assets/vedio/vd2.mp4" type="video/mp4" />
+                    Your browser does not support the video tag.
+                </video>
+                <div className="carousel-caption">
+                    <div className="p-3" style={{ maxWidth: '900px' }}>
+                        <h4 className="text-white text-uppercase fw-bold mb-4" style={{ letterSpacing: '3px' }}>Explore The World</h4>
+                        <h1 className="display-2 text-capitalize text-white mb-4">Let's The World Together!</h1>
+                        <p className="mb-5 fs-5">
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                        </p>
+                        <div className="d-flex align-items-center justify-content-center">
+                            <Link className="btn-hover-bg btn btn-primary rounded-pill text-white py-3 px-5" to="#">Discover Now</Link>
                         </div>
                     </div>
-                    <button className="carousel-control-prev" type="button" data-bs-target="#carouselId" data-bs-slide="prev">
-                        <span className="carousel-control-prev-icon btn bg-primary" aria-hidden="true"></span>
-                        <span className="visually-hidden">Previous</span>
-                    </button>
-                    <button className="carousel-control-next" type="button" data-bs-target="#carouselId" data-bs-slide="next">
-                        <span className="carousel-control-next-icon btn bg-primary" aria-hidden="true"></span>
-                        <span className="visually-hidden">Next</span>
-                    </button>
                 </div>
             </div>
         </div>
