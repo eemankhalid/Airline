67bec66c9aa14152914ff1f0fbabfba86636e8eb
eemankhalid <eemankhalid04@gmail.com>
2024-08-07 15:51:18 +0500

    comitting working code

--- airline/src/components/Navbar.jsx
+++ airline/src/components/Navbar.jsx
@@ -1,58 +1,60 @@
 import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
 
 const Navbar = () => {
+    const location = useLocation();
     return (
         <div className="container-fluid position-relative p-0">
             <nav className="navbar navbar-expand-lg navbar-light px-4 px-lg-5 py-3 py-lg-0">
-                <a href="" className="navbar-brand p-0">
+                <Link to="/" className="navbar-brand p-0">
                     <h1 className="m-0"><i className="fa fa-map-marker-alt me-3"></i>Travela</h1>
-                </a>
+                </Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
                     <span className="fa fa-bars"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarCollapse">
                     <div className="navbar-nav ms-auto py-0">
-                    <a href="/" className="nav-item nav-link active">Home</a>
+                        <Link to="/" className="nav-item nav-link active">Home</Link>
                         <div className="nav-item dropdown">
-                            <a href="/plan" className="nav-link dropdown-toggle" data-bs-toggle="dropdown">Plan</a>
+                            <Link to="/plan" className="nav-link dropdown-toggle" data-bs-toggle="dropdown">Plan</Link>
                             <div className="dropdown-menu m-0">
-                                <a href="/destination" className="dropdown-item">Book a Flight</a>
-                                <a href="/destination" className="dropdown-item">Group Travel Requests</a>
-                                <a href="/tour" className="dropdown-item">Charter Flight Requests</a>
-                                <a href="/tour" className="dropdown-item">Sky Cafe</a>
+                                <Link to="/destination" className="dropdown-item">Book a Flight</Link>
+                                <Link to="/destination" className="dropdown-item">Group Travel Requests</Link>
+                                <Link to="/tour" className="dropdown-item">Charter Flight Requests</Link>
+                                <Link to="/tour" className="dropdown-item">Sky Cafe</Link>
                             </div>
                         </div>
                         <div className="nav-item dropdown">
-                            <a href="#" className="nav-link dropdown-toggle" data-bs-toggle="dropdown">Manage</a>
+                            <Link to="#" className="nav-link dropdown-toggle" data-bs-toggle="dropdown">Manage</Link>
                             <div className="dropdown-menu m-0">
-                                <a href="/destination" className="dropdown-item">Modify Flight</a>
-                                <a href="/tour" className="dropdown-item">Cancel Flight</a>
-                                <a href="/booking" className="dropdown-item">Add AirRewards ID</a>
-                                <a href="/gallery" className="dropdown-item">Check Flight Status</a>
+                                <Link to="/destination" className="dropdown-item">Modify Flight</Link>
+                                <Link to="/tour" className="dropdown-item">Cancel Flight</Link>
+                                <Link to="/booking" className="dropdown-item">Add AirRewards ID</Link>
+                                <Link to="/gallery" className="dropdown-item">Check Flight Status</Link>
                             </div>
                         </div>
                         <div className="nav-item dropdown">
-                            <a href="#" className="nav-link dropdown-toggle" data-bs-toggle="dropdown">Add Extras</a>
+                            <Link to="#" className="nav-link dropdown-toggle" data-bs-toggle="dropdown">Add Extras</Link>
                             <div className="dropdown-menu m-0">
-                                <a href="/destination" className="dropdown-item">Add Baggage</a>
-                                <a href="/tour" className="dropdown-item">Add Seats</a>
-                                <a href="/booking" className="dropdown-item">Add Meals</a>
+                                <Link to="/destination" className="dropdown-item">Add Baggage</Link>
+                                <Link to="/tour" className="dropdown-item">Add Seats</Link>
+                                <Link to="/booking" className="dropdown-item">Add Meals</Link>
                             </div>
                         </div>
-                        <a href="/index" className="nav-item nav-link active">Check-In</a>
+                        <Link to="/index" className="nav-item nav-link active">Check-In</Link>
                         <div className="nav-item dropdown">
-                            <a href="#" className="nav-link dropdown-toggle" data-bs-toggle="dropdown">Air Rewards</a>
+                            <Link to="#" className="nav-link dropdown-toggle" data-bs-toggle="dropdown">Air Rewards</Link>
                             <div className="dropdown-menu m-0">
-                                <a href="/destination" className="dropdown-item">Login</a>
-                                <a href="/tour" className="dropdown-item">Join Rewards</a>
+                                <Link to="/destination" className="dropdown-item">Login</Link>
+                                <Link to="/tour" className="dropdown-item">Join Rewards</Link>
                             </div>
                         </div>
-                        <a href="/index" className="nav-item nav-link active">Blogs</a>
+                        <Link to="/index" className="nav-item nav-link active">Blogs</Link>
                         <div className="nav-item dropdown">
-                            <a href="#" className="nav-link dropdown-toggle" data-bs-toggle="dropdown">Help</a>
+                            <Link to="#" className="nav-link dropdown-toggle" data-bs-toggle="dropdown">Help</Link>
                             <div className="dropdown-menu m-0">
-                                <a href="/destination" className="dropdown-item">FAQs</a>
-                                <a href="/tour" className="dropdown-item">Contact Us</a>
+                                <Link to="/destination" className="dropdown-item">FAQs</Link>
+                                <Link to="/tour" className="dropdown-item">Contact Us</Link>
                             </div>
                         </div>
                     </div>
@@ -76,7 +78,7 @@ const Navbar = () => {
                                         Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
                                     </p>
                                     <div className="d-flex align-items-center justify-content-center">
-                                        <a className="btn-hover-bg btn btn-primary rounded-pill text-white py-3 px-5" href="#">Discover Now</a>
+                                        <Link className="btn-hover-bg btn btn-primary rounded-pill text-white py-3 px-5" to="#">Discover Now</Link>
                                     </div>
                                 </div>
                             </div>
@@ -91,7 +93,7 @@ const Navbar = () => {
                                         Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
                                     </p>
                                     <div className="d-flex align-items-center justify-content-center">
-                                        <a className="btn-hover-bg btn btn-primary rounded-pill text-white py-3 px-5" href="#">Discover Now</a>
+                                        <Link className="btn-hover-bg btn btn-primary rounded-pill text-white py-3 px-5" to="#">Discover Now</Link>
                                     </div>
                                 </div>
                             </div>
@@ -106,7 +108,7 @@ const Navbar = () => {
                                         Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
                                     </p>
                                     <div className="d-flex align-items-center justify-content-center">
-                                        <a className="btn-hover-bg btn btn-primary rounded-pill text-white py-3 px-5" href="#">Discover Now</a>
+                                        <Link className="btn-hover-bg btn btn-primary rounded-pill text-white py-3 px-5" to="#">Discover Now</Link>
                                     </div>
                                 </div>
                             </div>
