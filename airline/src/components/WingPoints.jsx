0a1967fec11215b60369d184e726b83990ef8996
mahaazkar321 <mahaazkar@gmail.com>
2024-08-08 12:31:33 +0500

    commiting wingpoints

--- airline/src/components/WingPoints.jsx
+++ airline/src/components/WingPoints.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from 'react';
+
 
 const WingPoints = () => {
-  return (
-    <div>
-      WingPoints
-    </div>
-  )
-}
+    return (
+        <div className="wing-points">
+            <div className="wing-points-content">
+                <h2>WingPoints – Loyalty Program</h2>
+                <p>
+                    WingPoints, our loyalty program, is the most generous loyalty program
+                    in the region. Based on a simple earn and redeem plan, WingPoints offers you
+                    the maximum value for points earned. Join now and discover new possibilities
+                    with WingPoints.
+                </p>
+                <button className="join-now-button">JOIN NOW</button>
+            </div>
+        </div>
+    );
+};
 
-export default WingPoints
+export default WingPoints;
