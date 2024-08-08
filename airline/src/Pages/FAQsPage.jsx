df67f57bc1f3f27526135ce8bc1e221273e38b6b
eemankhalid <eemankhalid04@gmail.com>
2024-08-08 12:42:11 +0500

    routing completed

---
+++ airline/src/Pages/FAQsPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FAQsPage = () => {
+  return (
+    <div>
+      FAQsPage
+    </div>
+  )
+}
+
+export default FAQsPage
---
+++ airline/src/Pages/ModifyFlightPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ModifyFlightPage = () => {
+  return (
+    <div>
+      ModifyFlightPage
+    </div>
+  )
+}
+
+export default ModifyFlightPage
