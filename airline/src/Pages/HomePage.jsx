02d8545a1ae04095770df2650af1162d9398809c
amnaahad <amnakhan200427@gmail.com>
2024-08-08 21:24:00 +0500

    bg color

--- airline/src/Pages/HomePage.jsx
+++ airline/src/Pages/HomePage.jsx
@@ -1,20 +1,26 @@
-import React from 'react'
-import WhereWeFly from '../components/WhereWeFly'
-import WingPoints from '../components/WingPoints'
-import FlightOptions from '../components/FlightOptions'
-import WhyInfinityWings from '../components/WhyInfinityWings'
-
+import React from 'react';
+import WhereWeFly from '../components/WhereWeFly';
+import WingPoints from '../components/WingPoints';
+import FlightOptions from '../components/FlightOptions';
+import WhyInfinityWings from '../components/WhyInfinityWings';
 
 const HomePage = () => {
   return (
-    <div>
-      <WhereWeFly />
-      <WingPoints />
-      <FlightOptions />
-      <WhyInfinityWings/>
-
+    <div style={{ backgroundColor: 'white', minHeight: '100vh', padding: '5px' }}>
+      <div style={{ margin: '0 auto', width: '98%' }}>
+        <WhereWeFly />
+      </div>
+      <div style={{ margin: '0 auto', width: '97%' }}>
+        <WingPoints />
+      </div>
+      <div style={{ margin: '0 auto', width: '97%' }}>
+        <FlightOptions />
+      </div>
+      <div style={{ margin: '0 auto', width: '97%' }}>
+        <WhyInfinityWings />
+      </div>
     </div>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
