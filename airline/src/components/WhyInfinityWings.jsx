cd1f7d0766bf11eef07f39bfa69701a196334ed5
eemankhalid <eemankhalid04@gmail.com>
2024-08-08 21:16:41 +0500

    commit changes

--- airline/src/components/WhyInfinityWings.jsx
+++ airline/src/components/WhyInfinityWings.jsx
@@ -7,16 +7,28 @@ const WhyInfinityWings = () => {
             <h2>Why Infinity Wings?</h2>
             <div className="why-infinity-wings">
                 <div className="feature">
-                    <img src="/src/assets/img/wimg1.jpeg" alt="Feature 1" />
-                    <p className='text'>"Infinity Wings prioritizes your comfort with outstanding customer service, ensuring a smooth and pleasant journey from start to finish. Experience unmatched care and attention on every flight."</p>
+                    <div className="feature-img-container">
+                        <img src="/src/assets/img/wimg1.jpeg" alt="Feature 1" />
+                    </div>
+                    <div className="feature-text-container">
+                        <p>"Infinity Wings prioritizes your comfort with outstanding customer service, ensuring a smooth and pleasant journey from start to finish. Experience unmatched care and attention on every flight."</p>
+                    </div>
                 </div>
                 <div className="feature">
-                    <img src="/src/assets/img/wimg2.jpeg" alt="Feature 2" />
-                    <p className='text'>"Fly with Infinity Wings' modern fleet, featuring cutting-edge technology, comfortable seating, and advanced safety features. Enjoy a secure and luxurious travel experience on all our flights."</p>
+                    <div className="feature-img-container">
+                        <img src="/src/assets/img/wimg2.jpeg" alt="Feature 2" />
+                    </div>
+                    <div className="feature-text-container">
+                        <p>"Fly with Infinity Wings' modern fleet, featuring cutting-edge technology, comfortable seating, and advanced safety features. Enjoy a secure and luxurious travel experience on all our flights."</p>
+                    </div>
                 </div>
                 <div className="feature">
-                    <img src="/src/assets/img/wimg3.jpeg" alt="Feature 3" />
-                    <p className='text'>"Infinity Wings connects you to the world with a vast network of destinations, offering convenient schedules and seamless travel experiences for both business and leisure travelers."</p>
+                    <div className="feature-img-container">
+                        <img src="/src/assets/img/wimg3.jpeg" alt="Feature 3" />
+                    </div>
+                    <div className="feature-text-container">
+                        <p>"Infinity Wings connects you to the world with a vast network of destinations, offering convenient schedules and seamless travel experiences for both business and leisure travelers."</p>
+                    </div>
                 </div>
             </div>
         </div>
