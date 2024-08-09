0da4112bbf58041b9e167fbfc0fa31769799d3ab
eemankhalid <eemankhalid04@gmail.com>
2024-08-09 15:00:58 +0500

    committing componenets

---
+++ airline/src/components/GroupTravelRequirements.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const GroupTravelRequirements = () => {
+  return (
+    <div>
+      GroupTravelRequirements
+    </div>
+  )
+}
+
+export default GroupTravelRequirements
