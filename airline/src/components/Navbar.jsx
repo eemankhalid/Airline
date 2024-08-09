a234314b57e915dae563b6773100a3fe9fd23e46
mahaazkar321 <mahaazkar@gmail.com>
2024-08-09 17:25:26 +0500

    commitnig

--- airline/src/components/Navbar.jsx
+++ airline/src/components/Navbar.jsx
@@ -74,29 +74,8 @@ const Navbar = () => {
             </nav>
 
 
-            <div className="hero-video-container">
-    <video className="hero-video" autoPlay loop muted>
-        <source src="src/assets/vedio/vd2.mp4" type="video/mp4" />
-        Your browser does not support the video tag.
-    </video>
-    <div className="carousel-caption wow animate__fadeIn">
-        <div className="p-3 wow animate__fadeInUp" style={{ maxWidth: '900px' }}>
-            <h4 className="text-white text-uppercase fw-bold mb-4 wow animate__fadeInDown" style={{ letterSpacing: '3px' }}>
-                Fly Beyond Limits with Infinity Wings
-            </h4>
-            <h2 className="display-2 text-capitalize text-white mb-4 wow animate__fadeInLeft">
-                Your Journey, Our Passion
-            </h2>
-            <p className="mb-5 fs-5 wow animate__fadeInRight">
-                At Infinity Wings, we prioritize your comfort and convenience. Enjoy top-notch amenities and exceptional service on every flight. Sign up for WingPoints and unlock exclusive benefits. Let's make your travel dreams a reality.
-            </p>
-            <div className="d-flex align-items-center justify-content-center wow animate__fadeInUp">
-                <Link className="btn" to="#">Discover More</Link>
-            </div>
+            
 
-        </div>
-    </div>
-</div>
 
         </div>
     );
