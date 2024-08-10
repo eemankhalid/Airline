6bf88523b2993b17fb1264c4f8a03156106d9d5a
eemankhalid <eemankhalid04@gmail.com>
2024-08-10 13:49:57 +0500

    comitting

--- airline/src/components/Hero.jsx
+++ airline/src/components/Hero.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
 
-const Hero = ({img,h4,h2,p,btn}) => {
+const Hero = ({style,img,h4,h2,p,btn}) => {
     return (
         <div className="hero-video-container">
             {img}
             <div className="carousel-caption">
-                <div className="p-3" style={{ maxWidth: '900px' }}>
+                <div className="p-3" style={style}>
                     <h4 className="text-white text-uppercase fw-bold mb-4" style={{ letterSpacing: '3px' }}>{h4}</h4>
                     <h2 className="display-2 text-capitalize text-white mb-4">{h2}</h2>
                     <p className="mb-5 fs-5">{p}</p>
