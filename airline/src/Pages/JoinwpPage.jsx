a3191d85823a984057dcafe888ce7294afdb55b6
mahaazkar321 <mahaazkar@gmail.com>
2024-08-15 21:38:39 +0500

    commiting changes

--- airline/src/Pages/JoinwpPage.jsx
+++ airline/src/Pages/JoinwpPage.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { useLocation } from 'react-router-dom';
 
 const JoinwpPage = () => {
@@ -7,18 +6,16 @@ const JoinwpPage = () => {
 
   return (
     <div className="joinwp-page">
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-      <h1>Congratulations!</h1>
-      <p>You are now a part of our WingPoints program.</p>
-      {userId && (
-        <div className="black-box">
-          <p>Your user ID is: <strong>{userId}</strong></p>
-          <p>Use this ID to log in to our website and enjoy exclusive benefits!</p>
-        </div>
-      )}
+      <div className="card">
+        <h1>Congratulations!</h1>
+        <p>You are now a part of our WingPoints program.</p>
+        {userId && (
+          <div className="black-box">
+            <p>Your user ID is: <strong>{userId}</strong></p>
+            <p>Use this ID to log in to our website and enjoy exclusive benefits!</p>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
