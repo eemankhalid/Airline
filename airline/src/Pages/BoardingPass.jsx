4b47d3dbcac41ac35114cabbab0b9f9fd0b787b1
eemankhalid <eemankhalid04@gmail.com>
2024-08-26 07:16:54 +0500

    comitting boarding pass

--- airline/src/Pages/BoardingPass.jsx
+++ airline/src/Pages/BoardingPass.jsx
@@ -1,15 +1,31 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import '../css/BoardingPass.css';
 
 const BoardingPass = () => {
   const { reservationId } = useParams();
 
   return (
-    <div>
-      <h2>Boarding Pass</h2>
-      <p>Reservation ID: {reservationId}</p>
-      {/* Display additional boarding pass details here */}
+    <>
+    <br/><br/><br/>
+    <div className="boarding-pass">
+      <div className="boarding-pass-info">
+        <p><strong>NAME OF PASSENGER:</strong> [Passenger Name]</p>
+        <p><strong>ORIGIN:</strong> [City] <span className="destination"> <strong>DESTINATION:</strong> [City]</span></p>
+        <p><strong>DATE:</strong> [XX/XX/XXXX] <span className="time"><strong>TIME:</strong> [# AM/PM]</span></p>
+        <p><strong>FLIGHT:</strong> [XX1234]<span className="time"><strong>Reservation Id:</strong> {reservationId}</span></p>
+        <p className="seat-number"><strong>SEAT:</strong> [17B]</p>
+      </div>
+      <div className="boarding-pass-side-info">
+        <div className="flight-code">ABC - ABC</div>
+        <p><strong>TO:</strong> [Name]</p>
+        <p><strong>FROM:</strong> [Name]</p>
+      </div>
+      {/* <div className="barcode">
+      </div> */}
+      <br/><br/><br/>
     </div>
+    </>
   );
 };
 
